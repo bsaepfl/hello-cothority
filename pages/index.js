4d5a2e8c10f9a326dd69ed8ccfe1f22f91300427
Louis Merlin <louis.merlin@epfl.ch>
2018-09-20 23:12:10 +0200

    Add standard linter

--- pages/index.js
+++ pages/index.js
@@ -1,15 +1,15 @@
 import { Component } from 'react'
-import { net } from '@dedis/cothority'
+// import { net } from '@dedis/cothority'
 import fetch from 'isomorphic-unfetch'
 
 export default class Index extends Component {
-  static async getInitialProps() {
+  static async getInitialProps () {
     const res = await fetch('https://skipchain.dedis.ch/index.js')
     const json = await res.json()
     return { servers: json.Blocks[0].Servers }
   }
 
-  render() {
+  render () {
     return (
       <div>
         <h3>Got a list of servers:</h3>
