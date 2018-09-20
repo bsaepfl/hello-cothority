d912baabb15c0e3db3731f6f2696aa6d77354bce
Louis Merlin <louis.merlin@epfl.ch>
2018-09-20 22:19:46 +0200

    Implement list of conode servers

--- pages/index.js
+++ pages/index.js
@@ -1,1 +1,24 @@
-export default () => <h1>Hello, Conode !</h1>
+import { Component } from 'react'
+import { net } from '@dedis/cothority'
+import fetch from 'isomorphic-unfetch'
+
+export default class Index extends Component {
+  static async getInitialProps() {
+    const res = await fetch('https://skipchain.dedis.ch/index.js')
+    const json = await res.json()
+    return { servers: json.Blocks[0].Servers }
+  }
+
+  render() {
+    return (
+      <div>
+        <h3>Got a list of servers:</h3>
+        <ul>
+          {this.props.servers.map(server => (
+            <li key={server}>{server}</li>
+          ))}
+        </ul>
+      </div>
+    )
+  }
+}
