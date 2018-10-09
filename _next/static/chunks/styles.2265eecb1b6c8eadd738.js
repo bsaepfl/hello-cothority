09e5a4050d68e833479ef9eabc3b6b131be779f4
Louis Merlin <louis.merlin@epfl.ch>
2018-10-09 17:29:42 +0200

    Updates

--- _next/static/chunks/styles.2265eecb1b6c8eadd738.js
+++
@@ -1,1 +0,0 @@
-(window.webpackJsonp=window.webpackJsonp||[]).push([[6],{537:function(n,w,o){}}]);
---
+++ precache-manifest.e5f837e4d01cacdef6cfefdfdb987ea3.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "f03ceaf1aa7b77da2ddeb37413571683",
+    "url": "/hello-cothority/static/media/unfetch.f03ceaf1.mjs"
+  },
+  {
+    "revision": "1b4452e7165c4ebd76fd",
+    "url": "/hello-cothority/static/js/runtime~main.1b4452e7.js"
+  },
+  {
+    "revision": "d90c367bde222465a25c",
+    "url": "/hello-cothority/static/js/main.d90c367b.chunk.js"
+  },
+  {
+    "revision": "bc3e088bddd1a52f8876",
+    "url": "/hello-cothority/static/js/1.bc3e088b.chunk.js"
+  },
+  {
+    "revision": "d90c367bde222465a25c",
+    "url": "/hello-cothority/static/css/main.449c3997.chunk.css"
+  },
+  {
+    "revision": "bc3e088bddd1a52f8876",
+    "url": "/hello-cothority/static/css/1.575ccb08.chunk.css"
+  },
+  {
+    "revision": "3e888cbc372539515d27947fe7c775f3",
+    "url": "/hello-cothority/index.html"
+  }
+];
